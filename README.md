8c4b068140c7e0be2edac0d44729acbce57449bb
cryocaustik <cryocaustik@rivendel>
2017-10-20 00:40:18 -0700

    initial

---
+++ README.md
@@ -0,0 +1,22 @@
+# py_mailer
+Python process to retreive bash mail from /var/mail/$USER and email it to a specified email addr.
+
+## Requirements
+- Python 3+
+- basic python packages normally shipped with python3.*
+    - smtplib
+    - email
+    - os
+    - subprocess
+- gmail email address 
+    - without two factor authentication
+
+## Setup & Run
+1. download file: `wget githuburl.com`
+1. edit file and fill in:
+    ```python
+    mail_from = 'email_to_send_from@gmail.com'
+    mail_passwd = 'gmail_passwd'
+    mail_to = 'email_to_send_to@someemail.com'
+    ```
+1. run: `python3 py_mailer.py`
---
+++ py_mailer.py
@@ -0,0 +1,64 @@
+#!/user/bin/python3
+
+from datetime import datetime
+from subprocess import getoutput
+from os import environ
+from os.path import getcwd, join
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+def py_mailer():
+    """
+    Function to obtain bash mail from /var/mail/$USER and send it in a email to specified email addr.
+    """
+    try:
+        mail_from = ''
+        mail_passwd = ''
+        mail_to = ''
+        mail_subj = 'bash mail {}'.format(datetime.strftime(datetime.now(), '%Y-%m-%d'))
+        mail_server = 'smtp.gmail.com'
+        mail_port = 587
+
+        get_mail_cmd = 'cat /var/mail/{}'.format(environ['USER'])
+        get_mail_cmd = 'cat /var/mail/$USER'
+        bash_respone = getoutput(get_mail_cmd)
+
+        if bash_respone:
+            mail_body = bash_respone
+        else:
+            mail_body = "no mail to report at this time"
+
+        msg = MIMEMultipart()
+        msg['From'] = mail_from
+        msg['To'] = mail_to
+        msg['Subject'] = mail_subj
+        msg.attach(MIMEText(mail_body, 'plain'))
+        mail_body = msg.as_string()
+
+        server = smtplib.SMTP(mail_server, mail_port)
+        server.starttls()
+        server.login(mail_from, mail_passwd)
+
+        server.sendmail(
+            from_addr=mail_from,
+            to_addrs=mail_to,
+            msg=mail_body,
+
+        )
+        server.quit()
+    except Exception as _err:
+        _msg = "main: error - type: {type} \t|\t{error}".format(
+            type=type(_err).__name__,
+            error=_err.args
+        )
+        print(_msg)
+        _path = join(getcwd(), 'logs/err_{}.log'.format(datetime.strftime(datetime.now(), '%Y-%m-%d')))
+        with open(_path, 'w') as _out:
+            _out.write(_msg)
+            _out.close()
+
+
+if __name__ == '__main__':
+    py_mailer()
